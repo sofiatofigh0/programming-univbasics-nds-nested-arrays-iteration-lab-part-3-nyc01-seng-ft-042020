4609f32502eea4466666c7e0e70ff866f91eb64e
sofia tofigh <sst2140@columbia.edu>
2020-04-02 00:53:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,10 +4,10 @@ def join_nested_strings(aoa)
  aoa.flatten!
   while counter1 < aoa.length do
       if aoa[counter1].class == String
-        phrase =aoa[counter1].join(' ')
+        phrase =aoa[counter1]
       new_array << phrase  
      end
      counter1=counter1+1 
     end  
- new_array
+ new_array.join(' ')
 end  
